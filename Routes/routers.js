c652ec053285756fbb5a886a647e0fcd668dd1be
GhulamShahbazali <shahbazamanat93@gmail.com>
2025-07-18 12:15:01 +0500

    some changing

--- Routes/routers.js
+++ Routes/routers.js
@@ -1,41 +1,39 @@
 const express = require('express');
+const router = express.Router();
 const User = require('../Models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const newImage=require('../MiddleWare/uploadImageMiddleWare');
-const  router = require('express').Router()
-
+const newImage = require('../MiddleWare/uploadImageMiddleWare');
 
 router.post('/regestierNewUser', newImage.single('userImage'), async (req, res) => {
-  const { userName, userPhone, userAddress, userEmail, userPassword } = req.body;
+  try {
+    const { userName, userPhone, userAddress, userEmail, userPassword } = req.body;
+    const imagePath = req.file ? req.file.path : '';
 
-  const imagePath = req.file ? req.file.path : "";
+    const existingUser = await User.findOne({ userEmail });
+    if (existingUser) {
+      return res.status(403).json({ message: 'User already exists', email: userEmail });
+    }
 
-  let user = await User.findOne({ userEmail });
-  if (user) {
-    return res.status(403).json({ message: "user already exists", email: "This " + userEmail });
-  } else {
-    user = new User({
+    const newUser = new User({
       userName,
       userPhone,
       userAddress,
       userEmail,
       userPassword,
-      userImage: imagePath, // ✅ Cloudinary URL
+      userImage: imagePath,
     });
-    await user.save();
+
+    await newUser.save();
 
     return res.status(200).json({
-      message: "User successfully added",
-      response: await User.find(),
+      message: 'User successfully added',
+      users: await User.find(),
     });
+  } catch (error) {
+    console.error('Registration Error:', error);
+    res.status(500).json({ message: 'Internal server error', error: error.message });
   }
 });
 
-
-
-router.get('/another-route' , (req , res)=>{
-    // router code here
-})
-
-module.exports  = router
+module.exports = router;
