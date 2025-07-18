0d7ab99fe18e84d0169ea8e0b539d6621678799f
GhulamShahbazali <shahbazamanat93@gmail.com>
2025-07-18 11:28:19 +0500

    Initial commit

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ database();
 app.use(express.json());
 app.use('/uploadImages', express.static('uploadImages'));
 app.use('/auth',router);
-
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
 app.listen(9000,()=>{
     console.log('server is runing on port 4000')
 })
